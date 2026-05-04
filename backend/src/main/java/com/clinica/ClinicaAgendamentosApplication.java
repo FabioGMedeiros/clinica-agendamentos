c5f05525c4089c75e19a3e7b612e53d3dc1e9fec
FabioGMedeiros <fabio.filho2001@hotmail.com>
2026-05-04 03:17:29 -0300

    feat: define fuso horário padrão como America/Sao_Paulo

--- backend/src/main/java/com/clinica/ClinicaAgendamentosApplication.java
+++ backend/src/main/java/com/clinica/ClinicaAgendamentosApplication.java
@@ -2,12 +2,14 @@ package com.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ClinicaAgendamentosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaAgendamentosApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
